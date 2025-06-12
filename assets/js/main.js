719cd252f3de63f2b2347ef516722348b76bcc0a
Joey Takeda <joeytakeda@users.noreply.github.com>
2025-06-12 11:32:23 -0700

    Stylistic updates to page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 document.querySelectorAll("a[href]").forEach((link) => {
   link.setAttribute("target", "_blank");
 });
-document.querySelectorAll("time[datetime]").forEach((el) => {
-  const date = new Date(`${el.dateTime}T12:00:00`);
-  const formatted = date.toLocaleString("en-US", {
-    month: "short",
-    day: "2-digit",
-    year: "numeric",
-  });
-  el.innerText = formatted;
-});
-const firstDetails = document.querySelector("details");
-if (firstDetails) {
-  firstDetails.open = true;
-}
+// document.querySelectorAll("time[datetime]").forEach((el) => {
+//   const date = new Date(`${el.dateTime}T12:00:00`);
+//   const formatted = date.toLocaleString("en-US", {
+//     month: "short",
+//     day: "2-digit",
+//     year: "numeric",
+//   });
+//   el.innerText = formatted;
+// });
+// const firstDetails = document.querySelector("details");
+// if (firstDetails) {
+//   firstDetails.open = true;
+// }
